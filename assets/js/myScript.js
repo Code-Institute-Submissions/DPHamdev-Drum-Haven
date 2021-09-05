d6897da84f3bf5650505004a72e2471a3830ea4f
DPHamdev <dphamill@gmail.com>
2021-09-05 02:26:06 +0000

    http.server

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -22,86 +22,7 @@ $( "#drumServ" ).mouseleave(function() {$( "#drumServ-text" ).hide() });
 $( "#cymRep" ).mouseenter(function() {$( "#cymRep-text" ).show() });
 $( "#cymRep" ).mouseleave(function() {$( "#cymRep-text" ).hide() });
 
-/* Drum Tuning Function */
-let kitSize = ["3 Piece", "4 Piece", "5 Piece", "6 Piece", "7 Piece", "8 Piece", "9 Piece", "10 Piece"];
-
-let bassDrum = [16, 18, 20, 22, 24, 26];
-let tom = [6, 8, 10, 12, 13, 14, 15, 16, 18,];
-let snare = [10, 12, 13, 14, 15];
-
-/* let tuneForm = document.createElement("form");
-tuneForm.innerHTML = `
-        <label for="fuName">Full Name</label>
-        <br>
-        <input type="text" name="fuName">
-        <br>
-        <label for="brand">Brand</label>
-        <br>
-        <input type="text" name="brand">
-        <br>
-            <label for="bDrum">Bass Drum</label>
-            <br>
-            <label for="bDrum">16"</label>
-            <input type="checkbox" name="bDrum" id="bDrum16" value="16">
-            <label for="bDrum">18"</label>
-            <input type="checkbox" name="bDrum" id="bDrum18" value="18">
-            <label for="bDrum">20"</label>
-            <input type="checkbox" name="bDrum" id="bDrum20" value="20">
-            <label for="bDrum">22"</label>
-            <input type="checkbox" name="bDrum" id="bDrum22" value="22">
-            <label for="bDrum">24"</label>
-            <input type="checkbox" name="bDrum" id="bDrum24" value="24">
-            <label for="bDrum">26"</label>
-            <input type="checkbox" name="bDrum" id="bDrum26" value="26">
-            <br>
-                <label for="bDrum">Tom Drum</label>
-                <br>
-                <label for="bDrum6">6"</label>
-                <input type="checkbox" name="tDrum6" value="6">
-                <label for="bDrum8">8"</label>
-                <input type="checkbox" name="tDrum8" value="8">
-                <label for="bDrum10">10"</label>
-                <input type="checkbox" name="tDrum10" value="10">
-                <label for="bDrum12">12"</label>
-                <input type="checkbox" name="tDrum12" value="12">
-                <label for="bDrum13">13"</label>
-                <input type="checkbox" name="tDrum13" value="13">
-                <label for="bDrum14">14"</label>
-                <input type="checkbox" name="tDrum14" value="14">
-                <label for="bDrum15">15"</label>
-                <input type="checkbox" name="tDrum15" value="15">
-                <label for="bDrum16">16"</label>
-                <input type="checkbox" name="tDrum16" value="16">
-                <label for="bDrum18">18"</label>
-                <input type="checkbox" name="tDrum18" value="18">
-                    <br>
-                    <label for="bDrum">Snare Drum</label>
-                    <br>
-                    <label for="bDrum10">10"</label>
-                    <input type="checkbox" name="sDrum10" value="10">
-                    <label for="bDrum12">12"</label>
-                    <input type="checkbox" name="sDrum12" value="12">
-                    <label for="bDrum13">13"</label>
-                    <input type="checkbox" name="sDrum13" value="13">
-                    <label for="bDrum14">14"</label>
-                    <input type="checkbox" name="sDrum14" value="14">
-                    <label for="bDrum15">15"</label>
-                    <input type="checkbox" name="sDrum15" value="15">
-                    <br>
-                        <label for="notes">Notes</label>
-                        <br>
-                        <input type="text" name="notes">
-                            <br>
-                            <br>
-                            <input action="/form.html" method="post" type="submit" onclick="check();" value="Tune my drums!">
-`
-document.getElementById("drum-tuning").appendChild(tuneForm);
-
-$("#tuneSubmit").click(function tuneSubmit() {
-    $("input:checkbox[name=bDrum]:checked").each(function(){
-        bassDrum.push($(this).val());
-    }); return bassDrum
-}) */
+/* Drum Tuning Form Function */
 
 let tuningForm = document.getElementById('tuning-form');
 tuningForm.addEventListener('submit', handleSubmitTune);
@@ -115,10 +36,10 @@ function handleSubmitTune(event) {
     let tuneFullName = tuningForm.elements['tuneFuName'].value 
     let bassDrum = tuningForm.elements['bDrum'].value
     let tuneBrand = tuningForm.elements['tuneBrand'].value
-    let tomDrum = tuningForm.elements['tDrum'].value
+    let tomNotes = tuningForm.elements['tomNotes'].value
     let snareDrum = tuningForm.elements['sDrum'].value
-    let notes = tuningForm.elements['tuneNotes'].value
     let kSize = tuningForm.elements['kitSize'].value
+    let price = document.getElementById('tunePrice').innerHTML
 
 
     let alertHtml = `Thank you ${tuneFullName} for submitting your tuning request. 
@@ -127,32 +48,25 @@ function handleSubmitTune(event) {
     Kit Size: ${kSize}
     Brand: ${tuneBrand}
     Bass Drum: ${bassDrum}
-    Tom Drums: ${tomDrum}
     Snare Drum: ${snareDrum}
-    Any additional notes you've included: 
-    ${notes}
+    Your tom sizes are:
+    ${tomNotes}
+
+    The price will be ${price}
+
     We'll respond to you soon`;
 
     window.alert(alertHtml);
     
-    return false;
 }
 
-/* let person = [];
 
-var x = 0;
-var array = Array();
 
-function add_element_to_array()
-{
- array[x] = document.getElementById("bDrum").value;
- $("input:checkbox[name=bDrum]:checked").each(function(){
-    person.push($(this).val());
-});
- alert("Element: " + array[x] + " Added at index " + x);
- x++;
- document.getElementById("bDrum").value = "";
- return person
-}
 
-console.log(person) */
+let tprice = {"4-Piece":"£40","5-Piece":"£50"};
+
+$(function(){
+    $('select[name=kitSize]').change(function(){
+      document.getElementById('tunePrice').textContent = tprice[$(this).val()];
+    });
+  });
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -14,3 +14,24 @@ function sendForm(contactForm) {
     );
     return false;  // To block from loading a new page
 }
+
+function sendTuneForm(contactForm) {
+    emailjs.send('Gmail', 'Tuning', {
+        "from_name": contactForm.tuneFuName.value,
+        "from_email": contactForm.tuneEmail.value,
+        "kitsize": contactForm.kitSize.value,
+        "bassdrum": contactForm.bDrum.value,
+        "snaredrum": contactForm.sDrum.value,
+        "tomsizes": contactForm.tomNotes.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
